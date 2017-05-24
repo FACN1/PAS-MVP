fa2b7a37aa635941ca83efe3cd1baebf5b6f6dbf
philawsophizing <philawsophizing@gmail.com>
2017-05-24 10:52:53 +0300

    Add a route handler for wild card routes. Close #15

--- src/routes.jsx
+++ src/routes.jsx
@@ -2,6 +2,12 @@ import React from 'react';
 import { Route, browserHistory } from 'react-router-dom';
 import App from './app.jsx';
 
-const Routes = () => <Route exact path='/' component={App} history={browserHistory} />;
+const Routes = () =>
+  (
+    <div>
+      <Route exact path='/' component={App} history={browserHistory} />
+      <Route path='/*' component={App} history={browserHistory} />
+    </div>
+  );
 
 export default Routes;
