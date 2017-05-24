34e985b1ead5a497236f76301b0ae239c83102e4
philawsophizing <philawsophizing@gmail.com>
2017-05-24 17:11:06 +0300

    remove app.jsx, import counter into routes

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
-import { Route, browserHistory } from 'react-router-dom';
-import App from './app.jsx';
+import { Route, browserHistory, Switch } from 'react-router-dom';
+import Counter from './components/counter.jsx';
 
 const Routes = () =>
   (
-    <div>
-      <Route exact path='/' component={App} history={browserHistory} />
-      <Route path='/*' component={App} history={browserHistory} />
-    </div>
+    <Switch>
+      <Route exact path='/' component={Counter} history={browserHistory} />
+      <Route exact path='/*' component={Counter} history={browserHistory} />
+    </Switch>
   );
 
 export default Routes;
