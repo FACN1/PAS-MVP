02643e7c183d1895fe8f5b06d7ab0097ed0624f2
philawsophizing <philawsophizing@gmail.com>
2017-05-28 12:38:45 +0300

    Create static container for selecting guesthouse ref #23

---
+++ src/containers/select_guesthouse.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import Header from '../components/header.jsx';
+import TextBox from '../components/text_box.jsx';
+import SearchBar from '../components/search_bar.jsx';
+
+const SelectGuesthouse = () =>
+  (
+    <div>
+      <Header />
+      <TextBox />
+      <SearchBar />
+    </div>
+  );
+
+export default SelectGuesthouse;
