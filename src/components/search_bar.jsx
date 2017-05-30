3ae69f6951148e4e952f0d012e4a866477b53842
Mario <mario.saliba98@gmail.com>
2017-05-30 11:03:25 +0300

    use react select for search input and auto complete #30

--- src/components/search_bar.jsx
+++ src/components/search_bar.jsx
@@ -1,12 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import Select from 'react-select';
 
-const SearchBar = () =>
-  (
-    <form>
-    <input type="text" />
-    <Link to="/method"><input type="submit" /></Link>
-    </form>
-  );
+class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      term: '',
+    };
+  }
+
+  logChange(val) {
+    console.log(val);
+  }
+
+
+  render() {
+    const names = Object.keys(this.props.guesthouses);
+    const options = names.map((currentValue, index) => {
+       {
+         value: index,
+         label: currentValue,
+       };
+    });
+    return (
+      <div>
+        <Select
+        options={options}
+        onChange={this.logChange}
+        />
+        <Link to="/method"><input type="submit" /></Link>
+      </div>
+    );
+  }
+}
 
 export default SearchBar;
