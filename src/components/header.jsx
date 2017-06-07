77cbfe3867cfe7e8ed6b57131aed8d2c651ac1bf
philawsophizing <philawsophizing@gmail.com>
2017-06-07 12:47:23 +0300

    Have header update with store

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
+import Store from '../state/store';
 
-const Header = () =>
-  (
+const Header = () => {
+  // console.log(props);
+  return (
     <div>
-      <h1>PAS</h1>
+      <h1>{Store.title}</h1>
       <img />
       <div className="line-break"></div>
-      <h2>Park & Sleep</h2>
+      <h2>{Store.tagLine}</h2>
     </div>
   );
+};
 
 
 export default Header;
