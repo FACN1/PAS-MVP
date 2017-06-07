9c11f7df6a1cd874d00c583c22a98b789359b234
philawsophizing <philawsophizing@gmail.com>
2017-06-07 12:52:20 +0300

    Remove browser router from each route, every route has it in index.jsx

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, browserHistory, Switch } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 
 import SelectGuesthouse from './containers/select_guesthouse.jsx';
 import TravelMethod from './containers/travel_method.jsx';
@@ -11,12 +11,12 @@ import Success from './containers/success.jsx';
 const Routes = () =>
   (
     <Switch>
-      <Route exact path='/method' component={TravelMethod} history={browserHistory} />
-      <Route exact path='/map' component={MapRoute} history={browserHistory} />
-      <Route exact path='/walking' component={WalkingDirections} history={browserHistory} />
-      <Route exact path='/success' component={Success} history={browserHistory} />
-      <Route exact path='/' component={SelectGuesthouse} history={browserHistory} />
-      <Route path='/*' component={SelectGuesthouse} history={browserHistory} />
+      <Route exact path='/method' component={TravelMethod} />
+      <Route exact path='/map' component={MapRoute} />
+      <Route exact path='/walking' component={WalkingDirections} />
+      <Route exact path='/success' component={Success} />
+      <Route exact path='/' component={SelectGuesthouse} />
+      <Route path='/*' component={SelectGuesthouse} />
     </Switch>
   );
 
