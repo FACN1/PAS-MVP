79f5b6d906badf7f6265db2fdfb3399364b05002
philawsophizing <philawsophizing@gmail.com>
2017-06-07 14:54:37 +0300

    Import and render SVG icons

--- src/components/button_container.jsx
+++ src/components/button_container.jsx
@@ -1,12 +1,22 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import carSvg from '../../public/images/car.svg';
+import busSvg from '../../public/images/bus.svg';
+import taxiSvg from '../../public/images/taxi.svg';
+
 const ButtonContainer = () =>
   (
     <div>
-      <Link to="/map"><img src="#" alt="car picture"/></Link>
-      <Link to="/map"><img src="#" alt="bus picture"/></Link>
-      <Link to="/map"><img src="#" alt="taxi picture"/></Link>
+      <Link to='/map'>
+        <img src={carSvg} />
+      </Link>
+      <Link to='/map'>
+        <img src={busSvg} />
+      </Link>
+      <Link to='/map'>
+        <img src={taxiSvg} />
+      </Link>
     </div>
   );
 
